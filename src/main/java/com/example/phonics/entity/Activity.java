feec59c0106b50937dfad3e6fda37a48d14c9dc6
Ahmed <a.maghraby96@yahoo.com>
2025-01-26 14:50:44 +0200

    activity create

--- src/main/java/com/example/phonics/entity/Activity.java
+++ src/main/java/com/example/phonics/entity/Activity.java
@@ -14,17 +14,13 @@ public class Activity  extends  AuditableEntity{
     private String image;
     private  String answer;
     private String sound;
-    @ElementCollection // Maps the list directly
-    @CollectionTable(name = "example_word", joinColumns = @JoinColumn(name = "word_id"))
-    @Column(name = "word_value")
+
     private List<String> word;
-    @ElementCollection // Maps the list directly
-    @CollectionTable(name = "example_choice", joinColumns = @JoinColumn(name = "choice_id"))
-    @Column(name = "choice_value")
+
+
     private List<String> choices;
 
-    @CollectionTable(name = "example_missingLetters", joinColumns = @JoinColumn(name = "missingLetters_id"))
-    @Column(name = "missingLetters_value")
+
     private List<String> missingLetters;
     @Transient
     private boolean isCompleted;
