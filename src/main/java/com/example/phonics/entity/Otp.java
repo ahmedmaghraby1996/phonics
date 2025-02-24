7250013c36b393252b3cbdda25b603a1050a6dd1
Ahmed <a.maghraby96@yahoo.com>
2025-02-24 11:59:16 +0200

    add profile

--- src/main/java/com/example/phonics/entity/Otp.java
+++ src/main/java/com/example/phonics/entity/Otp.java
@@ -25,7 +25,7 @@ public class Otp extends AuditableEntity {
     @Enumerated(EnumType.STRING)
     private OtpType otpType;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     public User user;
 }
